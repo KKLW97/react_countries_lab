ebdd41d22e592d30ee996b0470f501a8b184a9e8
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-18 15:54:48 +0100

    adds fetchCountries method and Country.js

--- src/containers/CountriesContainers.js
+++ src/containers/CountriesContainers.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
+
 const CountriesContainer = () => {
+
+    // need to fetch countries as JSON format e.g name.common, flag, population
+    // display the countries as a list
+    // add a button or a checklist to mark the country as visited
+    // onClick to move the country from countriesList to visitedCountriesList
+
+    const [country, setCountry] = useState(null);
+
+    const fetchCountries = async () => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const jsonData = await response.json();
+        setCountry(jsonData);
+    }
+
+    useEffect(() => {
+        fetchCountries();
+    },[])
+
     return(
-        <p>Hello from CountriesContainer!</p>
+        <>
+            <p>Hello from CountriesContainer!</p>
+        </>
     );
 
 }
